887dc62efe852339618127c86805b56d31d990d4
Henrique V. Domingues <hvdomingues1@gmail.com>
2020-02-15 20:02:55 -0300

    Terminada classe CompanyTaxPayer

--- src/entities/CompanyTaxPayer.java
+++ src/entities/CompanyTaxPayer.java
@@ -1,5 +1,42 @@
 package entities;
 
-public class CompanyTaxPayer {
+public class CompanyTaxPayer extends TaxPayer{
 
+	
+	private Integer numberOfEmployees;
+	
+	public CompanyTaxPayer() {
+	}
+	
+	public CompanyTaxPayer(String name, Double anualIncome, Integer numberOfEmployees) {
+		super(name,anualIncome);
+		this.setNumberOfEmployees(numberOfEmployees);
+	}
+	
+	
+	
+	
+	
+	
+	
+	@Override
+	public Double taxesPaid() {
+		
+		if(numberOfEmployees > 10) {
+			taxesPaid = anualIncome * 0.14;
+		}else {
+			taxesPaid = anualIncome * 0.16;
+		}
+		
+		return taxesPaid;
+	}
+
+	public Integer getNumberOfEmployees() {
+		return numberOfEmployees;
+	}
+
+	public void setNumberOfEmployees(Integer numberOfEmployees) {
+		this.numberOfEmployees = numberOfEmployees;
+	}
+	
 }
--- src/entities/TaxPayer.java
+++ src/entities/TaxPayer.java
@@ -4,9 +4,18 @@ public abstract class TaxPayer {
 
 	protected String name;
 	protected Double anualIncome;
+	protected Double taxesPaid;
 	
 	public abstract Double taxesPaid();
 
+	public TaxPayer() {
+	}
+	
+	public TaxPayer(String name, Double anualIncome) {
+		this.name = name;
+		this.anualIncome = anualIncome;
+	}
+	
 	public String getName() {
 		return name;
 	}
@@ -21,6 +30,14 @@ public abstract class TaxPayer {
 
 	public void setAnualIncome(Double anualIncome) {
 		this.anualIncome = anualIncome;
+	}
+
+	public Double getTaxesPaid() {
+		return taxesPaid;
+	}
+
+	public void setTaxesPaid(Double taxesPaid) {
+		this.taxesPaid = taxesPaid;
 	}
 	
 	
