f5bbdf86b447401c6cae8a35d4945f5645004734
Henrique V. Domingues <hvdomingues1@gmail.com>
2020-02-15 20:10:50 -0300

    Criada classe IndividualTaxPayer

---
+++ src/entities/IndividualTaxPayer.java
@@ -0,0 +1,39 @@
+package entities;
+
+public class IndividualTaxPayer extends TaxPayer{
+
+	
+	private Double healthExpenditures;
+	
+	public IndividualTaxPayer() {
+	}
+	
+	public IndividualTaxPayer(String name, Double anualIncome, Double healthExpenditures) {
+		super(name, anualIncome);
+		this.healthExpenditures = healthExpenditures;
+	}
+	
+	
+	@Override
+	public Double taxesPaid() {
+		
+		if(anualIncome < 20000.00) {
+			taxesPaid = anualIncome * 0.15;
+		}else {
+			taxesPaid = anualIncome * 0.25;
+		}if(healthExpenditures > 0) {
+			taxesPaid -= healthExpenditures / 2;
+		}
+		
+		return null;
+	}
+
+	public Double getHealthExpenditures() {
+		return healthExpenditures;
+	}
+
+	public void setHealthExpenditures(Double healthExpenditures) {
+		this.healthExpenditures = healthExpenditures;
+	}
+	
+}
